b1f41413a7ce930eac8f059428b8210b38f5c103
Isaac-Chow <99174054+Isaac-Chow@users.noreply.github.com>
2022-12-02 22:06:31 +0800

    Web files

---
+++ main.js
@@ -0,0 +1,16 @@
+window.onload=function(){
+    const answer=document.getElementById("code")
+    const button=document.getElementById("button");
+    button.onclick=function(){}
+    
+    answer.onchange=function(e){
+        let number=9894078826;
+        if(e.target.value==number){
+            window.location.href="complete.html";
+        }
+        else{
+            alert("Wrong code, try again!")
+        }
+    }
+
+}
